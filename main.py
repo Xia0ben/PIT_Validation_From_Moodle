0e10693d5fb67b39bce585426fbe4cf1a9d6b9ee
Xia0ben <10845530+Xia0ben@users.noreply.github.com>
2019-10-02 17:14:34 +0200

    Add change from main if statement to main() function and feature to only look for a specific list of students

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ if __name__ == "__main__":
     pass
 """
 
+MAIN_FUNCTION ="""\
+def main():
+    pass
+"""
+
 CURRENT_FILE_ABS_PATH = path.dirname(path.abspath(__file__))
 RELATIVE_PATH_TO_DATA_FOLDER = "./data/"
 RELATIVE_PATH_TO_STUDENT_ANSWERS = "./student_answers/"
@@ -64,7 +69,31 @@ def fix_code_to_add_main(code):
     return astor.to_source(ast_tree, indent_with=' ' * 4, add_line_information=False)
 
 
-if __name__ == "__main__":
+def transform_main_to_function(code):
+    ast_tree = ast.parse(source=code, filename="<string>", mode="exec")
+
+    main_function_element = ast.parse(source=MAIN_FUNCTION, filename="<string>", mode="exec").body[0]
+    main_function_element.body.pop(0)
+
+    for python_code_body_element in ast_tree.body:
+        if isinstance(python_code_body_element, ast.If):
+            try:
+                if (python_code_body_element.test.comparators[0].s == "__main__"
+                        and python_code_body_element.test.left.id == "__name__"):
+                    main_if_element = python_code_body_element
+                    main_function_element.body = main_if_element.body
+                    ast_tree.body.remove(main_if_element)
+                    ast_tree.body.append(main_function_element)
+                    break
+            except AttributeError:
+                pass
+
+    return_code = astor.to_source(ast_tree, indent_with=' ' * 4, add_line_information=False)
+
+    return return_code
+
+
+def validate_answers_for_students_emails(students_emails):
     if not os.path.exists(ABS_PATH_TO_DATA_FOLDER):
         os.makedirs(ABS_PATH_TO_DATA_FOLDER)
         raise FileNotFoundError("You did not have a data folder at the project root: it has been created for you, \
@@ -76,6 +105,10 @@ if __name__ == "__main__":
     with open(ABS_PATH_TO_DATA_FILE) as json_file:
         data = json.load(json_file)
         for student_participation in data[0]:
+            # Only validate for given list of students
+            if student_participation[INDEX_TO_MAIL] not in students_emails:
+                continue
+
             counter = 1
             for i in range(INDEX_TO_FIRST_ANSWER, INDEX_TO_LAST_ANSWER + 1):
                 # Write student answer in python file
@@ -100,6 +133,24 @@ if __name__ == "__main__":
                             print("Fixing their code to allow for proper testing...")
                             student_answer_python_code = fix_code_to_add_main(student_answer_python_code)
                             print("...Code fixed !")
+
+                            # Change main "if" statement to a main() function for easier testing
+                            student_answer_python_code = transform_main_to_function(student_answer_python_code)
+
+                            # Write student code into python file
+                            student_answer_python_file.write(student_answer_python_code)
+
+                            # Get and run test module corresponding to question
+                            test_module_name = "verification_tests.question_" + str(counter) + "_test"
+
+                            test_module = importlib.import_module(test_module_name)
+
+                            test_suite = unittest.TestLoader().loadTestsFromModule(test_module)
+                            unittest.TextTestRunner(verbosity=2).run(test_suite)
+
+                            # Increment counter for next question loop
+                            counter += 1
+
                     except SyntaxError as e:
                         # If the code was not properly formatted and the AST could not be built...
                         print(
@@ -111,17 +162,9 @@ if __name__ == "__main__":
                         print("Printing said code... :")
                         print(student_answer_python_code)
 
-                    # Write student code into python file
-                    student_answer_python_file.write(student_answer_python_code)
 
-                # Get and run test module corresponding to question
-                test_module_name = "verification_tests.question_" + str(counter) + "_test"
-
-                test_module = importlib.import_module(test_module_name)
-
-                test_suite = unittest.TestLoader().loadTestsFromModule(test_module)
-                unittest.TextTestRunner(verbosity=2).run(test_suite)
-
-                # Increment counter for next question loop
-                counter += 1
-
+if __name__ == "__main__":
+    validate_answers_for_students_emails([
+        "jean.ribes@insa-lyon.fr",
+        "florian.bonnafond@insa-lyon.fr"
+    ])
