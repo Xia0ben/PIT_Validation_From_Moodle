0a5fd6c7f976093be6808ea72e3ce5efa17ae0e7
Guillaume Bono <guillaume.bono@inria.fr>
2019-10-02 22:01:00 +0200

    added test for q3 and q4 (wip on output filter for q4.. so much spam)

--- verification_tests/question_3_test.py
+++ verification_tests/question_3_test.py
@@ -1,11 +1,30 @@
 import unittest
+import unittest.mock
+import io
+import re
+import random
 
 from fixed_student_answers import question_3
 
 
 class Question3Test(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_func_name(self):
+        self.assertIn("minMaxMoy", dir(question_3))
+
+    def test_minMaxMoy(self):
+        for rand_list in ([random.randint(0,100) for _ in range(random.randint(5,40))] for _ in range(50)):
+            self.assertEqual(question_3.minMaxMoy(rand_list),
+                    (min(rand_list), max(rand_list), sum(rand_list) / len(rand_list)))
+
+    def test_main(self):
+        outmock = io.StringIO()
+        with unittest.mock.patch("sys.stdout", outmock):
+            question_3.main()
+        outmock.seek(0)
+        pat = re.compile(r"10.*20.*15")
+        for line in outmock:
+            res = pat.search(line)
+            self.assertIsNotNone(res)
 
 
 if __name__ == '__main__':
